19357b0897219a8f905e6a2d31e4621319659888
Matt87c <matthewclockel@yahoo.com>
2020-06-16 21:07:25 -0400

    updated

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    // public static void main(String[] args) {
+
+    public static Double getArea(Double radius) {
+        return Math.PI * Math.pow(radius, 2);
+       // return 3.14
+
+    }
+}
